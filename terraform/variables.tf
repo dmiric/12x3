5526d525edfd5390076161c504cafc454f947b1d
dmiric <damir.miric@gmail.com>
2024-07-22 19:08:09 +0200

    Neon

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,24 +1,19 @@
 variable "upstash_email" {
   type = string
-  sensitive = true
 }
 
 variable "upstash_api_key" {
   type = string
-  sensitive = true
 }
 
 variable "render_api_key" {
   type = string
-  sensitive = true
 }
 
 variable "render_owner_id" {
   type = string
-  sensitive = true
 }
 
 variable "neon_api_key" {
   type = string
-  sensitive = true
 }
